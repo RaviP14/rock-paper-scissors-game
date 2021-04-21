3bd0222a29431e3d655d01a2647d587a717651eb
RaviP14 <ravi.patel14@outlook.com>
2021-04-21 15:29:44 +0100

    1 round of functional rock, paper, scissors

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,11 +1,18 @@
+
+function computerPlay() {
+    var choice = ["rock", "paper", "scissors"]
+    var chosen = choice[Math.floor(Math.random()*choice.length)];
+     return chosen;
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
         return ("you Lose! Paper beats Rock")
+    } else {
+        return ("You Win!")
     }
 }
-function computerPlay {
-    choice = ["rock", "paper", "scissors"]
-}
+
 const playerSelection = "rock"
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection,computerSelection));
