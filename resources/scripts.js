c0d7316bef7b42cb088389fefe25b55fe1536013
RaviP14 <ravi.patel14@outlook.com>
2021-04-21 15:28:14 +0100

    made files & linked external js

---
+++ resources/scripts.js
@@ -0,0 +1,11 @@
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return ("you Lose! Paper beats Rock")
+    }
+}
+function computerPlay {
+    choice = ["rock", "paper", "scissors"]
+}
+const playerSelection = "rock"
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection,computerSelection));
