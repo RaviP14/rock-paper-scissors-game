fd87bdd1988aec2dde228322f131c5c6f01dd2bd
RaviP14 <ravi.patel14@outlook.com>
2021-04-24 16:53:51 +0100

    added choice, score and final winner/loser to console log

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,72 +1,64 @@
-
+//keep score of the game:
+let playerScore = 0;
+let computerScore = 0;
 //adding game function which returns playRound function. 
 function game() {
-    //a round of rock,paper or scissors consists of two player choosing one of these options, this will show their outcome.
+    //a round of rock,paper or scissors consists of two player choosing one of these options, this will add their result to the score.
     function playRound(playerSelection, computerSelection) {
         if (playerSelection == "rock" && computerSelection == "paper"){
-            return ("you Lose! Paper beats Rock")
+            computerScore++
         } else if (playerSelection == "rock" && computerSelection == "scissors") {
-            return ("You Win!")
+            playerScore++
         } else if (playerSelection == "rock" && computerSelection == "rock") {
             return("It\'s a draw")
         } else if (playerSelection == "paper" && computerSelection == "rock") {
-            return ("You Win!")
+            playerScore++
         } else if (playerSelection == "paper" && computerSelection == "scissors") {
-            return ("You Lose! Scissors beats Paper")
+            computerScore++
         } else if (playerSelection == "paper" && computerSelection == "paper") {
             return ("It\'s a draw")
         } else if (playerSelection == "scissors" && computerSelection == "rock") {
-            return ("You Lose! Rock beats Scissors")
+            computerScore++
         } else if (playerSelection == "scissors" && computerSelection == "paper") {
-            return ("You Win!")
+            playerScore++
         } else if (playerSelection == "scissors" && computerSelection == "scissors") {
             return ("It\'s a draw")
         }
+        console.log("P1 " + playerScore + " : " + computerScore + " PC");
     }
     //Randamoly assign a choice to the computer.
     function computerPlay() {
         let choice = ["rock", "paper", "scissors"]
         let chosen = choice[Math.floor(Math.random()*choice.length)];
+        console.log("I choose " + chosen);
         return chosen;
     }
     //to get the players choice i must promt a question from the player and record their choice in a variable.
     function getMychoice() {
         let mychoice = window.prompt("Choose rock, paper or scissors", "").toLowerCase();
         if ((mychoice === "rock") || (mychoice == "paper") || (mychoice == "scissors")) {
+            console.log("pc chooses " + mychoice);
             return mychoice;
         } else {
             getMychoice()
         }
     }
-    //record the results of iteration - record score then show winner & loser of game. (try inside loop or game function?)
-    function score() {
-        let playerScore = 0;
-        let computerScore = 0;
-        if (playRound === "You Win!") {
-            playerScore++
-        } else if ((playRound(playerScore, computerScore) === "You Lose! Rock beats Scissors") || (playRound(playerScore, computerScore) === "you Lose! Paper beats Rock" ) || (playRound(playerScore, computerScore) === "You Lose! Scissors beats Paper")) {
-            computerScore++
-        }
-        console.log(playerScore);
-        console.log(computerScore);
-    
-        if (playerScore >= 3) {
-            console.log("Final result: You are the Winner!")
-        } else if (computerScore >= 3) {
-            console.log("Final result: Unlucky, you lost :(")
-        } else {
-            console.log(" Final result: It\'s a draw")
-        }
-    }
 
     const playerSelection = getMychoice();
     const computerSelection = computerPlay();
-    console.log(score);
     return playRound(playerSelection,computerSelection)
 }
+
 //iterate over game to play 5 rounds.
 for (let i = 0; i < 5; i++) {
-    console.log(game());
+    console.log(game()); 
 }
 
-
+//overall winner & loser of game:
+if (playerScore >= 3) {
+    console.log("Final result: You are the Winner!")
+} else if (computerScore >= 3) {
+   console.log("Final result: Unlucky, you lost :(")
+} else {
+   console.log(" Final result: It\'s a draw")
+}
